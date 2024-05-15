8486b0a7925e51af143154f2d00929b06ef85a76
Tanish <tnsstuduo@gmail.com>
2024-05-15 21:17:36 +0530

    implement file structure

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif , } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] , variable: '--font-inter'});
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ['latin'],
+  weight: ['400','700'],
+  variable: '--font-imb-okex-serif'
+})
+
+export const metadata: Metadata = {
+  title: "Banker",
+  description: "A banking platform for everyone",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
+    </html>
+  );
+}
