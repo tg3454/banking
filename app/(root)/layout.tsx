778738fe5e6a50f0f3545631d129407650b393b0
Tanish <tnsstuduo@gmail.com>
2024-05-15 23:56:14 +0530

    kbjk

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   return (
     <main className="flex h-screen w-full font-inter">
         <Sidebar user = {loggedIn}/>
-        <div className="flex size-full flex-col font-inter">
+        <div className="flex size-full flex-col">
         <div className="root-layout">
           <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
           <div>
